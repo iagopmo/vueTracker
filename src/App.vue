b58c791a9d43c7badbff7a119b1c4efa6510af60
Iago <iagopmo@gmail.com>
2023-07-25 00:25:07 -0300

    adicionando barra lateral ao app e corrigindo font awesome

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <main class="columns is-gapless is-multiline">
     <div class="clumn is-one-quarter">
-      
+      <BarraLateralVue />
     </div>    
   </main>
 </template>
 
 <script lang="ts">
+  import { defineComponent } from 'vue';
+  import BarraLateralVue from './components/BarraLateral.vue'
 
+  export default defineComponent({
+    name: 'App',
+    components: {
+      BarraLateralVue
+    }
+  })
 </script>
 
 <style>
