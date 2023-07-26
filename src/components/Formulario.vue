3644609668ce5ca117dca1231e328e1ef0aa3ed8
Iago <iagopmo@gmail.com>
2023-07-26 19:45:02 -0300

    criando metodo para iniciar contagem

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -7,15 +7,17 @@
             <div class="column">
                 <div class="is-flex is-align-items-center is-justify-content-space-between">
                     <section>
-                        <strong>00:00:00</strong>
+                        <strong>
+                            {{ tempoDecorrido }}
+                        </strong>
                     </section>
-                    <button class="button">
+                    <button class="button" @click="iniciar">
                         <span class="icon">
                             <i class="fas fa-play"></i>
                         </span>
                         <span>play</span>
                     </button>
-                    <button class="button">
+                    <button class="button" @click="finalizar">
                         <span class="icon">
                             <i class="fas fa-stop"></i>
                         </span>
@@ -31,7 +33,28 @@
     import { defineComponent } from 'vue'
 
     export default defineComponent({
-        name: "FormularioVue"
+        name: "FormularioVue",
+        data() {
+            return {
+                tempoEmSegundos: 0
+            }
+        },
+        computed: {
+            tempoDecorrido() : string {
+                return new Date(this.tempoEmSegundos * 1000).toISOString().substring(11,19)
+            }
+        },
+        methods: {
+            iniciar() {
+                setInterval(() => {
+                    this.tempoEmSegundos += 1
+                }, 1000)
+                console.log("Iniciando")
+            },
+            finalizar() {
+                console.log("finalizando")
+            }
+        }
     })
 </script>
 
