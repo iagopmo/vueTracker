ed9a3194bd080fb8244a8158f6b9c9c8dcb74772
Iago <iagopmo@gmail.com>
2023-07-25 22:29:31 -0300

    incluindo Formulario no app

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,23 @@
   <main class="columns is-gapless is-multiline">
     <div class="clumn is-one-quarter">
       <BarraLateralVue />
-    </div>    
+    </div>   
+    <div class="column is-three-quarter">
+      <FormularioVue />
+    </div> 
   </main>
 </template>
 
 <script lang="ts">
   import { defineComponent } from 'vue';
   import BarraLateralVue from './components/BarraLateral.vue'
+  import FormularioVue from './components/Formulario.vue'
 
   export default defineComponent({
     name: 'App',
     components: {
-      BarraLateralVue
+      BarraLateralVue,
+      FormularioVue
     }
   })
 </script>
