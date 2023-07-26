de8b39e441d8fdee66f942e48e85dacb5d0619d5
Iago <iagopmo@gmail.com>
2023-07-25 22:10:38 -0300

    criação componente formulario

---
+++ src/components/Formulario.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="box">
+        <div class="columns">
+            <div class="column is-8" role="form" aria-label="Formulario para criação de uma nova tarefa">
+                <input type="text" class="input" placeholder="Qual tarefa você deseja incluir?">
+            </div>
+            <div class="column">
+                <section>
+                    <strong>00:00:00</strong>
+                </section>
+                <button class="button">
+                    <span class="icon">
+                        <i class="fas fa-play"></i>
+                    </span>
+                    <span>play</span>
+                </button>
+                <button class="button">
+                    <span class="icon">
+                        <i class="fas fa-stop"></i>
+                    </span>
+                    <span>stop</span>
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue'
+
+    export default defineComponent({
+        name: "FormularioVue"
+    })
+</script>
+
+<style>
+
+</style>
