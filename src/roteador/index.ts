7b89b966fd5a84949119772c3dd48c3933e1b30e
Iago <iagopmo@gmail.com>
2023-08-02 20:08:32 -0300

    criacao view e rota para projetos

--- src/roteador/index.ts
+++ src/roteador/index.ts
@@ -1,11 +1,19 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router"
 import Tarefas from '../views/Tarefas.vue'
+import ProjetosView from '../views/Projetos.vue'
 
-const rotas: RouteRecordRaw[] = [{
-    path: '/',
-    name: 'Tarefas',
-    component: Tarefas
-}]
+const rotas: RouteRecordRaw[] = [
+    {
+        path: '/',
+        name: 'Tarefas',
+        component: Tarefas
+    },
+    {
+        path: '/projetos',
+        name: 'Projetos',
+        component: ProjetosView
+    }
+]
 
 const roteador = createRouter({
     history: createWebHashHistory(),
