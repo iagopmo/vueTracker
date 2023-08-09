066cfe01cc4a23728479b6600359b9af6bfe8811
Iago <iagopmo@gmail.com>
2023-08-09 00:16:31 -0300

    excluindo projetos e organizando as mutation

--- src/views/projetos/Formulario.vue
+++ src/views/projetos/Formulario.vue
@@ -23,6 +23,7 @@
 <script lang="ts">
 import { useStore } from '@/store'
 import { defineComponent } from 'vue'
+import { ALTERA_PROJETO, ADICIONA_PROJETO} from '../../store/tipo-mutacoes'
 
 export default defineComponent({
     name: 'FormularioView',
@@ -45,12 +46,12 @@ export default defineComponent({
     methods: {
         salvar() {
             if(this.id) {
-                this.store.commit('ALTERA_PROJETO', {
+                this.store.commit(ALTERA_PROJETO, {
                     id: this.id,
                     nome: this.nomeDoProjeto
                 })
             } else {
-                this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto)
+                this.store.commit(ADICIONA_PROJETO, this.nomeDoProjeto)
             }
             this.nomeDoProjeto = '',
             this.$router.push('/projetos')
--- src/views/projetos/Lista.vue
+++ src/views/projetos/Lista.vue
@@ -24,6 +24,11 @@
                               <i class="fas fa-pencil-alt"></i>
                             </span>
                         </router-link>
+                        <button class="button ml-2 is-danger" @click="excluir(projeto.id)">
+                            <span class="icon is-">
+                                <i class="fas fa fa-trash"></i>
+                            </span>
+                        </button>
                     </td>
                 </tr>
             </tbody>
@@ -34,13 +39,20 @@
 <script lang="ts">
 import { useStore } from '@/store'
 import { computed, defineComponent } from 'vue'
+import { EXCLUIR_PROJETO } from '../../store/tipo-mutacoes'
 
 export default defineComponent({
     name: 'ListaView',
+    methods: {
+        excluir(id: string) {
+            this.store.commit(EXCLUIR_PROJETO, id)
+        }
+    },
     setup() {
         const store = useStore()
         return {            
-            projetos: computed(() => store.state.projetos)
+            projetos: computed(() => store.state.projetos),
+            store
         }
     }
 })
