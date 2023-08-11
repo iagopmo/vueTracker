ddeef6d506b29ab1f2f00c4e3d73719a2ca4727f
Iago <iagopmo@gmail.com>
2023-08-10 23:35:57 -0300

    removendo automaticamente e mensagens com uso da store e mutation

--- src/views/projetos/Formulario.vue
+++ src/views/projetos/Formulario.vue
@@ -23,7 +23,8 @@
 <script lang="ts">
 import { useStore } from '@/store'
 import { defineComponent } from 'vue'
-import { ALTERA_PROJETO, ADICIONA_PROJETO} from '../../store/tipo-mutacoes'
+import { ALTERA_PROJETO, ADICIONA_PROJETO, NOTIFICAR} from '../../store/tipo-mutacoes'
+import { TipoNotificacao } from '@/interfaces/INotificacao'
 
 export default defineComponent({
     name: 'FormularioView',
@@ -54,6 +55,11 @@ export default defineComponent({
                 this.store.commit(ADICIONA_PROJETO, this.nomeDoProjeto)
             }
             this.nomeDoProjeto = '',
+            this.store.commit(NOTIFICAR, {
+                titulo: 'Novo projeto foi salvo',
+                texto: 'Pronto seu projeto ja esta disponivel',
+                tipo: TipoNotificacao.SUCESSO
+            })
             this.$router.push('/projetos')
         }
     },
