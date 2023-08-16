956d4791d8ee7eec606c28fc863ecac15e87d933
Iago <iagopmo@gmail.com>
2023-08-15 22:09:53 -0300

    notificando com mixins outra forma de notificar reaproveitacao codigo

--- src/views/projetos/Formulario.vue
+++ src/views/projetos/Formulario.vue
@@ -23,8 +23,9 @@
 <script lang="ts">
 import { useStore } from '@/store'
 import { defineComponent } from 'vue'
-import { ALTERA_PROJETO, ADICIONA_PROJETO, NOTIFICAR} from '../../store/tipo-mutacoes'
+import { ALTERA_PROJETO, ADICIONA_PROJETO} from '../../store/tipo-mutacoes'
 import { TipoNotificacao } from '@/interfaces/INotificacao'
+import { notificacaoMixin } from '@/mixins/notificar'
 
 export default defineComponent({
     name: 'FormularioView',
@@ -33,6 +34,7 @@ export default defineComponent({
             type: String
         }
     },
+    mixins: [notificacaoMixin],
     mounted() {
         if(this.id) {
             const projeto = this.store.state.projetos.find(proj => proj.id == this.id)
@@ -55,11 +57,7 @@ export default defineComponent({
                 this.store.commit(ADICIONA_PROJETO, this.nomeDoProjeto)
             }
             this.nomeDoProjeto = '',
-            this.store.commit(NOTIFICAR, {
-                titulo: 'Novo projeto foi salvo',
-                texto: 'Pronto seu projeto ja esta disponivel',
-                tipo: TipoNotificacao.SUCESSO
-            })
+            this.notificar(TipoNotificacao.SUCESSO, 'Excelente!', 'O projeto foi cadastrado com sucesso!')
             this.$router.push('/projetos')
         }
     },
